777fd63600d8aa750b691da3d78723c6712804c3
Cristian <crigropi@gmail.com>
2026-06-09 13:14:17 -0400

    Aplicación de constructores y getters en las clases creadas

--- src/cl/icel/pasteleria/Boleta.java
+++ src/cl/icel/pasteleria/Boleta.java
@@ -1,4 +1,23 @@
 package cl.icel.pasteleria;
+import java.time.LocalDate;
 
-public class Boleta {
+public
+class Boleta {
+    private String UUID;
+    private String nombreVendedor;
+    private LocalDate fechaDeVenta;
+    private int totalVenta;
+
+    /* Constructor del objeto Boleta
+     * @param UUID: uuid de la transaccion, es unico
+     * @param nombreVendedor: nombre del vendedor que realizo la venta
+     * @param fechaDeVenta: fecha en la que se realizo la venta
+     * @param totalVenta: total de la venta realizada
+     */
+    public Boleta(String UUID, String nombreVendedor, LocalDate fechaDeVenta, int totalVenta) {
+        this.UUID = UUID;
+        this.nombreVendedor = nombreVendedor;
+        this.fechaDeVenta = fechaDeVenta;
+        this.totalVenta = totalVenta;
+    }
 }
